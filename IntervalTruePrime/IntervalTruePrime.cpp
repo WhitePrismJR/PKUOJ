dba2b3f4c6da8e47bd3508b322dc51272dc9fa89
walterzhaoJR <519362600@qq.com>
2019-01-17 00:33:18 +0800

    change IntervalTruePrime algorithm

--- IntervalTruePrime/IntervalTruePrime.cpp
+++ IntervalTruePrime/IntervalTruePrime.cpp
@@ -9,38 +9,38 @@ using namespace std;
 
 bool isPrime(int n)
 {
-	for (int i = 2; i <=sqrt(n); i++)
-	{
-		if (n%i == 0)
-		{
-			return false;
-		}
-	}
-	return true;
+    for (int i = 2; i <=sqrt(n); i++)
+    {
+        if (n%i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 
 int Reverse(int n)
 {
-	std::string temp;
-	std::stringstream ss;
-	ss << n;
-	ss >> temp;
-	reverse(temp.begin(), temp.end());
-	int t = atoi(temp.c_str());
-	return t;
+    std::string temp;
+    std::stringstream ss;
+    ss << n;
+    ss >> temp;
+    reverse(temp.begin(), temp.end());
+    int t = atoi(temp.c_str());
+    return t;
 }
 
 int main()
 {
-	int M, N;
-	cin >> M >> N;
-	std::string c;
+    int M, N;
+    cin >> M >> N;
+    std::string c;
     std::vector<int> num;
     int i_rev = 0;
 
-	for (int i = M; i <= N; i++)
-	{
+    for (int i = M; i <= N; i++)
+    {
         i_rev = Reverse(i);
         if(num.end() != find(num.begin(),num.end(),i)){//这个数字已经找过了，不要再计算了
             continue;
@@ -50,20 +50,20 @@ int main()
             continue;
         }
 
-		if (isPrime(i) && isPrime(i_rev))
-		{
+        if (isPrime(i) && isPrime(i_rev))
+        {
             num.push_back(i);
-		    if(i != i_rev && i_rev <= N){//避免重复
+            if(i != i_rev && i_rev <= N){//避免重复
                 num.push_back(i_rev); 
             }
-		}
-	}
-    
-    
+        }
+    }
+
+
     sort(num.begin(),num.end());
     for(int i=0;i<num.size();i++){
         cout << num[i] << " ";
     }
-	
+
     return 0;
 }
